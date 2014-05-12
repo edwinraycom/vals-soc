a462326145970f3ff271e6a8268a5c22f9c17a75
ps3comps3com <ps3comps3com@googlemail.com>
2014-05-12 20:01:38 +0000

    Initial view for projects, searchable by tag and ordered by tag and/or organisation.

---
+++ actions/project_actions.php
@@ -0,0 +1,50 @@
+<?php
+define('DRUPAL_ROOT', realpath(getcwd().'/../../../../..'));
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);//Used to be DRUPAL_BOOTSTRAP_SESSION
+module_load_include('php', 'vals_soc', 'includes/classes/Project');
+
+switch ($_GET['action']){
+	case 'list_projects':
+		try{
+			$tags=null;
+			if(isset($_POST['tags'])){
+				$tags = $_POST['tags'];
+			}
+			$organisation=null;
+			if(isset($_POST['organisation'])){
+				$organisation = $_POST['organisation'];
+			}
+			//Return result to jTable
+			$jTableResult = array();
+			$jTableResult['Result'] = "OK";
+			$jTableResult['TotalRecordCount'] = Project::getInstance()->getProjectsRowCountBySearchCriteria(
+					$tags, $organisation);
+			$jTableResult['Records'] = Project::getInstance()->getProjectsBySearchCriteria($tags,
+					$organisation, $_GET["jtSorting"], $_GET["jtStartIndex"], $_GET["jtPageSize"]);
+			print json_encode($jTableResult);
+		}
+		catch(Exception $ex){
+			//Return error message
+			$jTableResult = array();
+			$jTableResult['Result'] = "ERROR";
+			$jTableResult['Message'] = $ex->getMessage();
+			print json_encode($jTableResult);
+		}
+	break;
+	case 'project_detail':
+		$project_id=null;
+		if(isset($_GET['project_id'])){
+			$results = Project::getInstance()->getProjectbyId($_GET['project_id']);
+			$projectDetail = array();
+			$projectDetail['title'] = $results->title;
+			$projectDetail['description'] = $results->description;
+			print json_encode($results);
+		}
+		else{
+			echo "No Project identifier submitted!";
+		}
+	break;
+	
+	default: echo "No such action: ".$_GET['action'];
+}
--- includes/classes/Project.php
+++ includes/classes/Project.php
@@ -1,9 +1,6 @@
 <?php
-
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+*
  */
 class Project {
 	
@@ -19,5 +16,44 @@ class Project {
     public function getProjects(){
     	$projects = db_select('soc_projects')->fields('soc_projects')->execute()->fetchAll(PDO::FETCH_ASSOC);
     	return $projects;
+    }
+    
+    public function getProjectbyId($id){
+    	$project = db_select('soc_projects')->fields('soc_projects')->condition('pid', $id)->execute()->fetchAll(PDO::FETCH_ASSOC);
+    	return $project;
+    }
+    
+    public function getProjectsRowCountBySearchCriteria($tags, $organisation){
+    	$projectCount = db_select('soc_projects');
+    	if(isset($tags)){
+    		$projectCount->condition('tags', '%'.$tags.'%', 'LIKE');
+    	}
+    	if(isset($organisation) && $organisation !="0"){
+    		$projectCount->condition('oid', $organisation);
+    	}
+    	$projectCount->fields('soc_projects');
+    	return $projectCount->execute()->rowCount();
+    }
+    
+    public function getProjectsBySearchCriteria($tags, $organisation, $sorting, $startIndex, $pageSize){
+    	$queryString = "SELECT p.pid, p.title, o.name, p.description, p.tags"
+    			." FROM soc_projects p, soc_organisations o"
+    			." WHERE p.oid = o.org_id";
+    	if(isset($tags)){
+    		$queryString .=	 " AND tags LIKE '%".$tags."%'";
+    	}
+    	if(isset($organisation) && $organisation !="0"){
+    		$queryString .=	 " AND p.oid = ".$organisation;
+    	}
+    	$queryString .= 	 " ORDER BY " . $sorting
+    	." LIMIT " . $startIndex . "," . $pageSize . ";";
+    	
+    	$result = db_query($queryString);
+    	
+    	$rows = array();
+    	foreach ($result as $record) {
+    		$rows[] = $record;
+    	}
+    	return $rows;
     }
 }
