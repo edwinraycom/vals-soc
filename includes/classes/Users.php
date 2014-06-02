33f98fa47183f66a48d77643f060a238b8e6f2b6
ps3comps3com <ps3comps3com@googlemail.com>
2014-06-02 19:39:55 +0000

    Added missing method

--- includes/classes/Users.php
+++ includes/classes/Users.php
@@ -70,6 +70,17 @@ class Users {
 		return $students;
 	}
 
+	static function keyField($type){
+		switch ($type){
+			case 'group': return 'group_id';break;
+			case 'institute': return 'inst_id';break;
+			case 'organisation': return 'org_id';break;
+			case 'project': return 'pid';break;
+			case 'proposal': return 'propid';break;
+			default: return '';
+		}
+	}
+	
 	/*
 	 * Get Users of type member_type out of the organisation structure of type group_type, based on:
 	 * a) group_id == all => get all students, tutors etc.
