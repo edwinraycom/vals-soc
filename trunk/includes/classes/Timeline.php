7f5db6307b30dd5713b0abd8127c6542c108b368
ps3comps3com@googlemail.com <ps3comps3com@googlemail.com@c407e9b9-25d2-593d-d33e-dcd4f528bc98>
2014-06-16 22:09:20 +0000

    Added a testing facility which allows the admin (for now) to change where the timeline is up to by pretending TODAY is another date - useful for rewinding/forward-winding through the process. (& will help to demo)

--- trunk/includes/classes/Timeline.php
+++ trunk/includes/classes/Timeline.php
@@ -2,21 +2,27 @@
 module_load_include('php', 'vals_soc', 'includes/classes/StatelessTimeline');
 class Timeline {
 
-	public static function getInstance(){
-		// uncomment line below to use test dates 
-		// - setting when NOW is between page requests
-		//$timelineTestDate = "2014-03-04 16:14:15";
-		if(isset($timelineTestDate)){
+	public static function getInstance($timelineTestDate=null){
+		Timeline::setupSession($timelineTestDate);
+		return StatelessTimeline::getInstance();
+	}
+	
+	private static function setupSession($timelineTestDate){
+		if(!isset($_SESSION)){
 			session_id('TimelineMultipageSession');
-			if(!isset($_SESSION)){
-				session_start();
-			}
-			if (!isset($_SESSION['timelineDate'])){
-				$_SESSION['timelineDate'] = $timelineTestDate;
-			}
+			session_start();
+		}
+		if(isset($timelineTestDate)){
+			$_SESSION['timelineDate'] = $timelineTestDate;
+			StatelessTimeline::getInstance()->setDummyTestDate($_SESSION['timelineDate']);
+			watchdog('setting NOW from new value', $timelineTestDate);
+			return;
+		}
+		if (isset($_SESSION['timelineDate'])){
+			watchdog('setting NOW from value found in session',$_SESSION['timelineDate']);
 			StatelessTimeline::getInstance()->setDummyTestDate($_SESSION['timelineDate']);
 		}
-		return StatelessTimeline::getInstance();
+		
 	}
 	
 }
