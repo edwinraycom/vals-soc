4694d1d32e6439f84657e4911bf6c9201e18aa3a
edwin <edwin@raycom.com>
2014-06-26 10:05:25 +0000

    Implemented the constant _VALS_SOC_URL (either /vals or empty), removed some unnecessary includes

---
+++ initial.php
@@ -0,0 +1,19 @@
+<?php
+//Copy this file to the root of the Drupal installation
+function getInitialPath($path1, $path2){
+	$pos = strpos($path1, $path2);
+	if (! $pos) {
+		$pos = strpos($path1, '/index.php');
+		if (!$pos){
+			return '';
+		} else {
+			$arr1 = str_split($path1, $pos);
+			return $arr1[0];
+		}	
+	}
+	$arr1 = str_split($path1, $pos);
+	return $arr1[0];
+}
+
+$vals = getInitialPath($_SERVER['PHP_SELF'], '/sites/all/modules');
+define('_VALS_SOC_URL', $vals);
