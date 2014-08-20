64d26b9ae2130f8e4b63517e9100495f2f8178c4
edwin@raycom.com <edwin@raycom.com@c407e9b9-25d2-593d-d33e-dcd4f528bc98>
2014-08-20 15:05:08 +0000

    I could not reproduce it, but sometimes Drupal seems to rebuild the form and then the #attached has a predefined meaning. According docs I prefix it with module name to have a custom form attribute read by renderform.
    Also: it is now possible to run multiple tests. See test_functions for example

--- trunk/includes/js/test_functions.js
+++ trunk/includes/js/test_functions.js
@@ -1,37 +1,73 @@
+/*
+ * Some form testing functions -only defined once.
+ * by having a #vals_soc_attached attribute in the form we can set extra tests in the variable
+ * vals_extra_tests like: 
+ * $form['#vals_soc_attached']['js'] = array(
+		array(
+			'type'  => 'file',
+			'data' => '/includes/js/test_functions.js',
+		),
+// 		array(
+// 			'type' => 'direct',	
+// 			'data'=> 'vals_extra_tests = ["test2"];',
+// 		),
+	);
+ */
 var testing = testing || {
-	test_required_fields : function (form_selector, msg_target){		
-		var msgs = '';
+	default_tests : ['test_required_fields'],
+	//msgs : '',
+	msgs : [],
+	warning_style : {'border-style'  :'solid', 'border-color' : 'orange' },
+	none_style : {'border-style'  :'', 'border-color' : '' },
+	run : function (env, msg_target){
+		var tests = this.default_tests;
 		var all_good = true;
-		var warning_style = {'border-style'  :'solid', 'border-color' : 'orange' };
-		var none_style = {'border-style'  :'', 'border-color' : '' };
+		if (typeof vals_extra_tests != 'undefined'){
+			tests = tests.concat(vals_extra_tests);
+		}
+		var test = '';
+		for (var i = 0; i < tests.length; i++) {
+		    test = tests[i];
+			all_good = all_good && testing[test](env, msg_target);
+		}
+		if (all_good === false) {
+			if (!this.msgs){
+				this.msgs = ['Your form is not correct'];
+			}
+			ajaxError(msg_target, this.msgs.join("<br/>"));
+			console.log(this.msgs.join("\n"));
+		}
+		this.msgs = [];
+		return all_good;
+	},
+	test2 : function (env, targ){
+		this.msgs.push(' Test 2 done');
+		return true;
+	},
+	test_required_fields : function (form_selector, msg_target){	
+		var all_good = true;
 		var self = this;
 		$jq("#" + form_selector+ " .required").each(function( index ) {
 			if ($jq(this).prop("tagName") == 'TEXTAREA'){
 				var rte_id = '#cke_' + $jq(this).prop("id");
-				if (self.isBlank($jq( this ).val())){
-					$jq(rte_id).css(warning_style);
+				if (self.isBlank($jq(this).val())){
+					$jq(rte_id).css(self.warning_style);
 					all_good = false;
 				} else {
-					$jq(rte_id).css(none_style);
+					$jq(rte_id).css(self.none_style);
 				}
 			} else {
 				if (self.isBlank($jq( this ).val())){
-					$jq(this).css(warning_style);
+					$jq(this).css(self.warning_style);
 					all_good = false;
 				} else {
-					$jq(this).css(none_style);
+					$jq(this).css(self.none_style);
 				}
 			}
 			
 		});
 		if (all_good === false) {
-			msgs = 'You have left some required fields open.';
-			ajaxError(msg_target, msgs);
-			console.log(msgs);
-//			all_good = true;
-//			return false;
-		} else {
-			//console.log('all is good');
+			this.msgs.push('You have left some required fields open.');
 		}
 		return all_good;
 	},
