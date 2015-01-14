7e618d1f3274f32422c1f4c08de12dbc6f79601a
edwin@raycom.com <edwin@raycom.com@c407e9b9-25d2-593d-d33e-dcd4f528bc98>
2015-01-14 09:16:03 +0000

    Made organisation dropdown alphabetical and solved bug in matching process

---
+++ tags/0.5.6.2/README.md
@@ -0,0 +1,29 @@
+vals_soc
+========
+
+VALS Drupal module
+
+Install the following Drupal Module Dependencies...
+
+[Admin Menus] (https://drupal.org/project/admin_menu)
+
+[Date Module](https://drupal.org/project/date)
+
+[SMTP Module](https://drupal.org/project/smtp)
+
+[CKEditor Module](https://drupal.org/project/ckeditor)
+
+Create a front page for the guests (node 1) and one for the logged in users (node 2).
+Go to Structure->blocks and enable the Semester of Code block (move it to the spot 'content' in
+the current theme).
+Go to settings and enable it for node 2 ('node/2') only
+Make this block available only for  authenticated users.
+
+Copy the file initial.php to the root of the installation. Due to a bug in Drupal it needs the base url before it can 
+do a bootstrap in ajax. The bootstrap from the index doesn't suffer from that. Probably because Drupal does not derive
+the base url (based on assumptions about the position of index.php if it is already defined. Instead we derive it ourselves
+from a location that is known before the bootstrap. All urls served pass either the (/vals)/index.php or
+the (/vals)/sites/all/modules/vals_soc/actions
+
+Copy the file index.php to the root of the installation. Also due to a combination of Drupal and server configuration
+the index needed a bug fix.
---
+++ tags/0.5.6.2/dev/deprecated/drupal_ajax_functions.js
@@ -0,0 +1,83 @@
+(function($, Drupal){
+/*
+	Drupal.behaviors.myBehavior = {
+			attach: function (context, settings) {
+				//Drupal.attachBehaviors();
+			}
+	}
+*/
+	Drupal.ajax.prototype.commands.jsEntitySaveCallback = function(ajax, response, status){
+		var key = response.key;
+		var tabTarget = response.tabTarget;
+		var entityType = response.entityType;
+		//var target_fun = 'refreshSingleTab';
+		if(key==''){
+			key = 0;
+			ajaxFormCall("vals-soc-" + entityType + "-form", 
+				"administration", 
+				"save",
+				{
+					type: entityType, 
+					id:0
+				}, 
+				"refreshTabs",
+				"json",
+				[entityType, tabTarget]
+			);
+		} else {
+			ajaxFormCall("vals-soc-" + entityType + "-form",
+				"administration",
+				"save",
+				{
+					type: entityType,
+					id: key,
+					target: tabTarget 
+				},
+				"refreshSingleTab",
+				"json",
+				[entityType, tabTarget]
+			);
+		}
+	};
+	// TODO - restructure & refine this OR remove if newer version of ajaxformcall plays nicer with drupal
+	Drupal.ajax.prototype.commands.timelineSaveCallback = function(ajax, response, status){
+		var key = response.key;
+		var tabTarget = response.tabTarget;
+		var entityType = response.entityType;;
+		if(key==''){
+			key = 0;
+			ajaxFormCall("vals-soc-" + entityType + "-form", 
+				"timeline", 
+				"save",
+				{
+					type: entityType, 
+					id:0
+				}, 
+				"refreshTabs",
+				"json",
+				[entityType, tabTarget, "timeline"]
+			);
+		} else {
+			ajaxFormCall("vals-soc-" + entityType + "-form",
+				"timeline",
+				"save",
+				{
+					type: entityType,
+					id: key,
+					target: tabTarget 
+				},
+				"refreshSingleTab",
+				"json",
+				[entityType, tabTarget, "timeline"]
+			);
+		}
+	};
+	/*
+	Drupal.ajax.prototype.commands.timelineSaveCallback = function(ajax, response, status){
+		var entityType="program_timeline";
+		var call_args = $jq('#' + "vals-soc-" + entityType + "-form").serialize();
+		console.log(call_args);
+		$jq.post( url("timeline","save"), call_args);
+	}
+	*/
+}(jQuery, Drupal));
